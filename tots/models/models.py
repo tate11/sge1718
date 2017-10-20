1d51010d888b0d5cdb2b6e721ea458e678462cf2
xxjcaxx <xxjcaxx@yahoo.es>
2017-10-20 18:00:38 +0000

    computed

--- tots/models/models.py
+++ tots/models/models.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from openerp.exceptions import ValidationError
 
 class net(models.Model):
      _name = 'tots.net'
 
      name = fields.Char()
+     net_ip = fields.Char()
      mask = fields.Integer()
      net_map = fields.Binary()
      net_class = fields.Selection([('a','A'),('b','B'),('c','C')])
@@ -15,9 +17,30 @@ class net(models.Model):
 class pc(models.Model):
      _name = 'tots.pc'
 
-     name = fields.Char()
+     name = fields.Char(default="PC")
+     number = fields.Integer()
+     ip = fields.Char(compute='_get_ip')
      ping = fields.Float()
-     registered = fields.Date()
-     uptime = fields.Datetime()
+
+     def _get_date(self):
+       print self
+       return fields.Date.today()
+
+
+     registered = fields.Date(default=_get_date)
+     uptime = fields.Datetime(default=lambda self: fields.Datetime.now())
      net = fields.Many2one('tots.net')
-     servers = fields.Many2many('tots.net',relation='net_servers')          
+     servers = fields.Many2many('tots.net',relation='net_servers')  
+
+     @api.depends('number','net')
+     def _get_ip(self):
+       print self
+       for pc in self:
+         print pc
+         pc.ip=str(pc.net.net_ip)+str(pc.number)
+
+     @api.constrains('number')
+     def _check_number(self):
+       for pc in self:
+        if pc.number > 254:
+            raise ValidationError("The IP must be under 254: %s" % pc.number)        
